8b2ca80a45bb13682375b0127852187936b8497c
RobinCHoffpauir <RobinCHoffpauir@users.noreply.github.com>
2021-12-23 11:36:28 +0000

    ✅ Ready to clone and code.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
 import sys
 import pytest
 
-from project_name import create_app
-from project_name.ext.commands import populate_db
-from project_name.ext.database import db
+from fuegoskrilla import create_app
+from fuegoskrilla.ext.commands import populate_db
+from fuegoskrilla.ext.database import db
 
 
 @pytest.fixture(scope="session")
